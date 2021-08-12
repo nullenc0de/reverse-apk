f10d34d53bc0b2280fd5e0973bf12f3278c3f2e5
nullenc0de <pseekamp@gmail.com>
2021-08-12 12:44:33 -0400

    Update all-apk.sh

--- all-apk.sh
+++ all-apk.sh
@@ -1,4 +1,5 @@
 bbscope bc -b |egrep 'play.google.com|android.com' |cut -d = -f2 |grep com. |cut -d '&' -f1 | cut -d ')' -f1 >scope.txt
 bbscope h1 -u nullenc0de -t <token> -b |egrep '\.android' |cut -d = -f2 |anew scope.txt
 
-cat scope.txt | while read apk ; do bash /opt/reverse-apk/reverse-apk.sh $apk > /dev/null; done
+cat scope.txt | while read apk ; do echo bash /opt/reverse-apk/reverse-apk.sh $apk ; done > runme.sh
+bash runme.sh
